951a0c62549571019b174eee5596f2c3ee63a8fa
gkpty <gkardonski@gmail.com>
2020-09-07 18:29:16 -0500

    fixed cache invalidations and upload updated

--- test/uploadUpdatedTest.js
+++ test/uploadUpdatedTest.js
@@ -1,7 +1,7 @@
 const {getFiles, uploadUpdatedFiles} = require('../lib/storage/upload')
 
 getFiles().then(files => {
-  uploadUpdatedFiles('localizehtml.com', files.files)
+  uploadUpdatedFiles('localizehtml.com', files.files, true)
   .then(data=>console.log(data))
   .catch(err => console.log(err))
 }).catch(err=> console.log(err))
